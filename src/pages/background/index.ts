5a7cc65d0b51337586f0e2472abe45c8697a18b5
opvel <opvel.hero@gmail.com>
2024-05-18 03:52:20 +0900

    feat: add chrome side panel behavior and message listener

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -10,3 +10,34 @@ reloadOnUpdate('pages/background');
 reloadOnUpdate('pages/content/style.scss');
 
 console.log('background loaded');
+
+chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch(error => console.error(error));
+
+reloadOnUpdate('pages/sidepanel');
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.type === 'getSelectedTextRequest') {
+    console.log('getSelectedTextRequest');
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      console.log('tabs', tabs);
+      chrome.scripting
+        .executeScript({
+          target: { tabId: tabs[0].id },
+          func: function getSelectedText() {
+            console.log('window', window);
+            console.log(window.getSelection().toString());
+            return window.getSelection().toString();
+          },
+        })
+        .then(result => {
+          console.log('result', result);
+          sendResponse({ response: result[0].result });
+        })
+        .catch(e => {
+          console.error(e);
+          sendResponse({ response: 'error' });
+        });
+    });
+    return true;
+  }
+});
