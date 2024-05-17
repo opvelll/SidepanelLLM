0821ba00e7d755421dec8a5bc13720bb18712db3
opvel <opvel.hero@gmail.com>
2024-05-18 03:47:04 +0900

    chore: update manifest permissions and add default title for action

--- manifest.js
+++ manifest.js
@@ -15,7 +15,9 @@ const manifest = {
   name: '__MSG_extensionName__',
   version: packageJson.version,
   description: '__MSG_extensionDescription__',
-  permissions: ['storage', 'sidePanel'],
+  permissions: ['storage', 'sidePanel', 'activeTab', 'scripting', 'tabs'],
+  host_permissions: ['<all_urls>'],
+
   side_panel: {
     default_path: 'src/pages/sidepanel/index.html',
   },
@@ -25,28 +27,29 @@ const manifest = {
     type: 'module',
   },
   action: {
-    default_popup: 'src/pages/popup/index.html',
+    // default_popup: 'src/pages/popup/index.html',
     default_icon: 'icon-34.png',
+    default_title: 'Click to open panel',
   },
-  chrome_url_overrides: {
-    newtab: 'src/pages/newtab/index.html',
-  },
+  // chrome_url_overrides: {
+  //   newtab: 'src/pages/newtab/index.html',
+  // },
   icons: {
     128: 'icon-128.png',
   },
-  content_scripts: [
-    {
-      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-      js: ['src/pages/contentInjected/index.js'],
-      // KEY for cache invalidation
-      css: ['assets/css/contentStyle<KEY>.chunk.css'],
-    },
-    {
-      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-      js: ['src/pages/contentUI/index.js'],
-    },
-  ],
-  devtools_page: 'src/pages/devtools/index.html',
+  // content_scripts: [
+  //   {
+  //     matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+  //     js: ['src/pages/contentInjected/index.js'],
+  //     // KEY for cache invalidation
+  //     css: ['assets/css/contentStyle<KEY>.chunk.css'],
+  //   },
+  //   {
+  //     matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+  //     js: ['src/pages/contentUI/index.js'],
+  //   },
+  // ],
+  //devtools_page: 'src/pages/devtools/index.html',
   web_accessible_resources: [
     {
       resources: ['assets/js/*.js', 'assets/css/*.css', 'icon-128.png', 'icon-34.png'],
