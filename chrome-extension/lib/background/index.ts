bae750ca6f808724af64bc62c8601cc600ad161b
opvel <opvel.hero@gmail.com>
2024-05-18 11:26:08 +0900

    chore: Remove unused SidePanel component and background script

--- chrome-extension/lib/background/index.ts
+++ chrome-extension/lib/background/index.ts
@@ -2,3 +2,32 @@ import 'webextension-polyfill';
 
 console.log('background loaded');
 console.log("Edit 'apps/chrome-extension/lib/background/index.ts' and save to reload.");
+
+chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch(error => console.error(error));
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.type === 'getSelectedTextRequest') {
+    console.log('getSelectedTextRequest');
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      console.log('tabs', tabs);
+      chrome.scripting
+        .executeScript({
+          target: { tabId: tabs[0].id },
+          func: function getSelectedText() {
+            console.log('window', window);
+            console.log(window.getSelection().toString());
+            return window.getSelection().toString();
+          },
+        })
+        .then(result => {
+          console.log('result', result);
+          sendResponse({ response: result[0].result });
+        })
+        .catch(e => {
+          console.error(e);
+          sendResponse({ response: 'error' });
+        });
+    });
+    return true;
+  }
+});
--- pages/sidepanel/src/SidePanel.tsx
+++ pages/sidepanel/src/SidePanel.tsx
@@ -1,54 +1,24 @@
 import '@src/SidePanel.css';
 import {
-  exampleThemeStorage,
-  useStorageSuspense,
   withErrorBoundary,
   withSuspense,
 } from '@chrome-extension-boilerplate/shared';
-import { ComponentPropsWithoutRef } from 'react';
+import { AIChatView } from 'react-ai-chat-view';
 
 const SidePanel = () => {
-  const theme = useStorageSuspense(exampleThemeStorage);
+  const systemPrompt = 'hello';
+  const fetchAIChatAPI = async () => {
+    return 'hello';
+  };
+  const handleGetSelectionButton = async (inputTextValue: string, setInputTextValue: (value: string) => void) => {
+    const res = await chrome.runtime.sendMessage({ type: 'getSelectedTextRequest' });
+    setInputTextValue('```\n' + res.response + '\n```\n');
+  };
 
   return (
-    <div
-      className="App"
-      style={{
-        backgroundColor: theme === 'light' ? '#eee' : '#222',
-      }}>
-      <header className="App-header" style={{ color: theme === 'light' ? '#222' : '#eee' }}>
-        <img src={chrome.runtime.getURL('sidepanel/logo.svg')} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>pages/sidepanel/src/SidePanel.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ color: theme === 'light' ? '#0281dc' : undefined, marginBottom: '10px' }}>
-          Learn React
-        </a>
-        <h6>The color of this paragraph is defined using SASS.</h6>
-        <ToggleButton>Toggle theme</ToggleButton>
-      </header>
+    <div>
+      <AIChatView {...{ systemPrompt, fetchAIChatAPI, handleGetSelectionButton }} />
     </div>
-  );
-};
-
-const ToggleButton = (props: ComponentPropsWithoutRef<'button'>) => {
-  const theme = useStorageSuspense(exampleThemeStorage);
-  return (
-    <button
-      className={
-        props.className +
-        ' ' +
-        'font-bold mt-4 py-1 px-4 rounded shadow hover:scale-105 ' +
-        (theme === 'light' ? 'bg-white text-black' : 'bg-black text-white')
-      }
-      onClick={exampleThemeStorage.toggle}>
-      {props.children}
-    </button>
   );
 };
 
