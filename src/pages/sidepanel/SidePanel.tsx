5a7bbca1e36ac99c01e129f840b65942d4965b5e
opvel <opvel.hero@gmail.com>
2024-05-18 21:01:35 +0900

    feat: update SidePanel component with AIChatView and fetchAIChatAPI improvements

--- src/pages/sidepanel/SidePanel.tsx
+++ src/pages/sidepanel/SidePanel.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import withSuspense from '@src/shared/hoc/withSuspense';
 import withErrorBoundary from '@src/shared/hoc/withErrorBoundary';
 import { AIChatView } from 'react-ai-chat-view';
+import { ChatContextType } from 'react-ai-chat-view/dist/components/ChatContextType';
 
 const SidePanel = () => {
   const systemPrompt = 'hello';
-  const fetchAIChatAPI = async () => {
-    return 'hello';
+  const fetchAIChatAPI = async (modelname: string, context: ChatContextType) => {
+    const res = await chrome.runtime.sendMessage({ type: 'queryChatAPI', model: modelname, context: context });
+    return res.response;
   };
   const handleGetSelectionButton = async (inputTextValue: string, setInputTextValue: (value: string) => void) => {
+    console.log('handleGetSelectionButton');
     const res = await chrome.runtime.sendMessage({ type: 'getSelectedTextRequest' });
     setInputTextValue('```\n' + res.response + '\n```\n');
   };
