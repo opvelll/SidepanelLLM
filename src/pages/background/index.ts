f6d3b070425873b82a4e98c112682757120a28a7
opvel <opvel.hero@gmail.com>
2024-05-18 21:00:02 +0900

    feat: add OpenAI chat API integration for background page

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -2,42 +2,57 @@ import reloadOnUpdate from 'virtual:reload-on-update-in-background-script';
 import 'webextension-polyfill';
 
 reloadOnUpdate('pages/background');
-
-/**
- * Extension reloading is necessary because the browser automatically caches the css.
- * If you do not use the css of the content script, please delete it.
- */
-reloadOnUpdate('pages/content/style.scss');
+import OpenAI from 'openai';
+import { ChatCompletion } from 'openai/resources';
+import OptionStorage from '@root/src/shared/storages/OptionStorage';
 
 console.log('background loaded');
 
 chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch(error => console.error(error));
 
 reloadOnUpdate('pages/sidepanel');
+let openai: OpenAI;
+OptionStorage.get().then(({ openAIKey, googleKey }) => {
+  openai = new OpenAI({
+    apiKey: openAIKey,
+  });
+  console.log(openAIKey, googleKey);
 
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.type === 'getSelectedTextRequest') {
-    console.log('getSelectedTextRequest');
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      console.log('tabs', tabs);
-      chrome.scripting
-        .executeScript({
-          target: { tabId: tabs[0].id },
-          func: function getSelectedText() {
-            console.log('window', window);
-            console.log(window.getSelection().toString());
-            return window.getSelection().toString();
-          },
-        })
-        .then(result => {
-          console.log('result', result);
-          sendResponse({ response: result[0].result });
-        })
-        .catch(e => {
-          console.error(e);
-          sendResponse({ response: 'error' });
-        });
-    });
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log('message', message);
+    if (message.type === 'getSelectedTextRequest') {
+      console.log('getSelectedTextRequest');
+      sendSelectionText(sendResponse);
+      return true;
+    } else if (message.type === 'queryChatAPI') {
+      fetchAIChatAPI(message.model, message.context, sendResponse);
+      return true;
+    }
     return true;
-  }
+  });
 });
+
+const fetchAIChatAPI = async (model, context, sendResponse) => {
+  const chatCompletion: ChatCompletion = await openai.chat.completions.create({
+    messages: context,
+    model: model,
+  });
+  sendResponse({ response: chatCompletion.choices[0].message.content });
+};
+
+const sendSelectionText = sendResponse => {
+  chrome.tabs.query({ active: true, currentWindow: true }, async tabs => {
+    try {
+      const result = await chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        func: function getSelectedText() {
+          return window.getSelection().toString();
+        },
+      });
+      sendResponse({ response: result[0].result });
+    } catch (e) {
+      console.error(e);
+      sendResponse({ response: 'error' });
+    }
+  });
+};
