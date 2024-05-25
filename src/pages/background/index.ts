eebe969952e66bcd1ff8e5bd9e71aceeb5ca96c4
opvel <opvel.hero@gmail.com>
2024-05-25 18:35:18 +0900

    feat: add YouTube subtitle fetching functionality

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -7,7 +7,9 @@ reloadOnUpdate('pages/sidepanel');
 import OpenAI from 'openai';
 import { ChatCompletion } from 'openai/resources';
 import OptionStorage from '@root/src/shared/storages/OptionStorage';
-import { TextSelectionSender } from './lib/TextSelectionSender';
+import getYoutubeSubtitles from './lib/YoutubeSubtitleFetcher';
+import getAllPageText from './lib/WebPageTextExtractor';
+import sendSelectionText from './lib/TextSelectionSender';
 
 chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch(error => console.error(error));
 
@@ -17,40 +19,29 @@ OptionStorage.get().then(({ openAIKey }) => {
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log('message', message);
     if (message.type === 'getSelectedTextRequest') {
-      TextSelectionSender.sendSelectionText(sendResponse);
+      sendSelectionText(sendResponse);
     } else if (message.type === 'queryChatAPI') {
       fetchAIChatAPI(openai, message.model, message.context, sendResponse);
     } else if (message.type === 'getAllPageRequest') {
       getAllPageText(sendResponse);
+    } else if (message.type === 'getSubtitlesRequest') {
+      getYoutubeSubtitles(sendResponse);
     }
     return true;
   });
 });
 
 const fetchAIChatAPI = async (openai, model, context, sendResponse) => {
-  const chatCompletion: ChatCompletion = await openai.chat.completions.create({
-    messages: context,
-    model: model,
-  });
-  sendResponse({ status: 'success', response: chatCompletion.choices[0].message.content });
-};
-
-const getAllPageText = async sendResponse => {
-  chrome.tabs.query({ active: true, currentWindow: true }, async tabs => {
-    if (!tabs[0].id) return sendResponse({ status: 'error', response: '', error: 'No active tab found.' });
-    const result = await chrome.scripting.executeScript({
-      target: { tabId: tabs[0].id },
-      func: getMainText,
+  try {
+    const chatCompletion: ChatCompletion = await openai.chat.completions.create({
+      messages: context,
+      model: model,
     });
-    if (result.length === 0 || !result[0].result)
-      return sendResponse({ status: 'error', response: '', error: 'No text found on the page.' });
-    sendResponse({ status: 'success', response: result[0].result });
-  });
+    sendResponse({ status: 'success', response: chatCompletion.choices[0].message.content });
+  } catch (e) {
+    console.error(e);
+    sendResponse({ status: 'error', errorMessage: e.message });
+  }
 };
-
-function getMainText() {
-  const mainElement = document.querySelector('main');
-  return mainElement ? mainElement.innerText : '';
-}
 
 console.log('background loaded');
