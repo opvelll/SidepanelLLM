23833760b15d840697e11d69973b9e441560d306
opvel <opvel.hero@gmail.com>
2024-05-29 22:06:25 +0900

    refactor: update SidePanel component imports and handle AIChatView response error

--- src/pages/sidepanel/SidePanel.tsx
+++ src/pages/sidepanel/SidePanel.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import withSuspense from '@src/shared/hoc/withSuspense';
 import withErrorBoundary from '@src/shared/hoc/withErrorBoundary';
 import { AIChatView } from 'react-ai-chat-view';
-import { ChatContextType } from 'react-ai-chat-view/dist/components/ChatContextType';
 import useStorage from '@root/src/shared/hooks/useStorage';
 import ApiKeyStorage from '@root/src/shared/storages/ApiKeyStorage';
 import { getActiveModelNames } from './lib/ModelFetcher';
@@ -10,13 +9,15 @@ import { GetTextRequest, ReceivedMessage, ResponseMessage } from './lib/MessageT
 import { MdOutlineSubtitles } from 'react-icons/md';
 import { SiPagekit } from 'react-icons/si';
 import { FaRegCopy } from 'react-icons/fa';
-import { ChatFormButtonData } from 'react-ai-chat-view/dist/components/ChatForm/ChatFormSideButton';
+import { ChatFormButtonData } from 'react-ai-chat-view/dist/components/ChatView/ChatForm/ChatFormSideButton';
+import { ChatContextType } from 'react-ai-chat-view/dist/components/ChatView/Type/ChatContextType';
 
 const SidePanel = () => {
   const systemPrompt = 'hello';
   const fetchAIChatAPI = async (modelname: string, context: ChatContextType) => {
     const res = await chrome.runtime.sendMessage({ type: 'queryChatAPI', model: modelname, context: context });
-    return res.response;
+    if (res.status === 'error') throw new Error(res.errorMessage);
+    return { content: res.response, tokenCount: res.completion_tokens, totalTokenCount: res.total_tokens };
   };
 
   const modelList = getActiveModelNames(useStorage(ApiKeyStorage));
@@ -25,19 +26,19 @@ const SidePanel = () => {
     {
       title: 'get selection',
       icon: <FaRegCopy />,
-      func: handleRequestButton.bind(null, 'getSelectedTextRequest', 'Selected Text:'),
+      func: handleRequestButton.bind(null, 'getSelectedTextRequest', 'Extracts from the website:'),
       color: 'text-orange-300',
     },
     {
       title: 'subtitles',
       icon: <MdOutlineSubtitles />,
-      func: handleRequestButton.bind(null, 'getSubtitlesRequest', 'Subtitles:'),
+      func: handleRequestButton.bind(null, 'getSubtitlesRequest', 'Subtitling information on youtube:'),
       color: 'text-red-400',
     },
     {
       title: 'all page',
       icon: <SiPagekit />,
-      func: handleRequestButton.bind(null, 'getAllPageRequest', 'All Page Text:'),
+      func: handleRequestButton.bind(null, 'getAllPageRequest', 'All sentences on page:'),
       color: 'text-gray-500',
     },
   ];
@@ -76,6 +77,6 @@ const handleRequestButton = async (
 };
 
 const formatResponse = (inputTextValue: string, formatString: string, res: ResponseMessage) =>
-  inputTextValue + '\n' + formatString + '\n```\n' + res.response + '\n```\n';
+  inputTextValue + '\n' + formatString + '\n```\n' + res.response + '\n```\n\n';
 
 export default withErrorBoundary(withSuspense(SidePanel, <div> Loading ... </div>), <div> Error Occur </div>);
