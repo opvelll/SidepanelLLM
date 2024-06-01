6fc87db0187a092b9696eddaa7ba13e72be96d24
opvel <opvel.hero@gmail.com>
2024-06-01 21:52:52 +0900

    refactor: update YoutubeSubtitleFetcher.ts to improve subtitle fetching logic

--- src/pages/background/lib/YoutubeSubtitleFetcher.ts
+++ src/pages/background/lib/YoutubeSubtitleFetcher.ts
@@ -3,26 +3,37 @@ import executeScript from './ChromeTabScriptExecutor';
 import { ReceivedMessage } from '../../sidepanel/lib/MessageType';
 
 const getYoutubeSubtitles = async () => {
-  return await executeScript(getVideoIdFromUrl, 'YoutubeアドレスからvideoIDを取り出せませんでした。', async result => {
-    try {
-      const subtitles = await getSubtitles({ videoID: result, lang: 'en' });
-      const concatenatedSubtitles = concatenateSubtitles(subtitles);
-      return { status: 'success', response: concatenatedSubtitles } as ReceivedMessage;
-    } catch (e) {
-      console.error(e);
-      return { status: 'error', errorMessage: e.message };
-    }
-  });
+  return await executeScript(
+    getVideoIdAndTitleFromUrl,
+    'YoutubeアドレスからvideoIDを取り出せませんでした。',
+    async result => {
+      try {
+        const subtitles = await getSubtitles({ videoID: result.videoId, lang: 'en' });
+        const concatenatedSubtitles = concatenateSubtitles(result.title, subtitles);
+        return { status: 'success', response: concatenatedSubtitles } as ReceivedMessage;
+      } catch (e) {
+        console.error(e + ' :en');
+        try {
+          const subtitles = await getSubtitles({ videoID: result.videoId, lang: 'ja' });
+          const concatenatedSubtitles = concatenateSubtitles(result.title, subtitles);
+          return { status: 'success', response: concatenatedSubtitles } as ReceivedMessage;
+        } catch (e) {
+          console.error(e + ' :ja');
+          return { status: 'error', errorMessage: e.message };
+        }
+      }
+    },
+  );
 };
 
-const getVideoIdFromUrl = () => {
-  const url = window.location.href;
-  console.log(url);
-  return new URLSearchParams(new URL(url).search).get('v');
+const getVideoIdAndTitleFromUrl = () => {
+  const videoId = new URLSearchParams(new URL(window.location.href).search).get('v');
+  const title = document.querySelector('title').innerText;
+  return { videoId, title };
 };
 
-const concatenateSubtitles = (subtitles: { text: string }[]): string => {
-  return subtitles.map(subtitle => subtitle.text).join('\n');
+const concatenateSubtitles = (title: string, subtitles: { text: string }[]): string => {
+  return 'title: ' + title + '\n\n' + subtitles.map(subtitle => subtitle.text).join('\n');
 };
 
 export default getYoutubeSubtitles;
