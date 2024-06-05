f753f4e481bcb5c3f65fbb8f9038f9d670c679cd
opvel <opvel.hero@gmail.com>
2024-06-05 22:56:55 +0900

    refactor: add OpenAI API key validation in fetchAIChatAPI function

--- src/pages/background/lib/ChatAPI.ts
+++ src/pages/background/lib/ChatAPI.ts
@@ -1,8 +1,17 @@
 import { ChatCompletion } from 'openai/resources';
 import { ReceivedMessage } from '../../sidepanel/lib/MessageType';
+import ApiKeyStorage from '@root/src/shared/storages/ApiKeyStorage';
+import OpenAI from 'openai';
 
-export const fetchAIChatAPI = async (openai, model, context): Promise<ReceivedMessage> => {
+export const fetchAIChatAPI = async (model, context): Promise<ReceivedMessage> => {
   try {
+    // 毎度、APIキーを取得して、new OpenAI() で初期化している
+    const { openAIKey } = await ApiKeyStorage.get();
+    if (!openAIKey) {
+      return { status: 'error', errorMessage: 'OpenAI API key is not set' };
+    }
+    const openai = new OpenAI({ apiKey: openAIKey });
+
     const chatCompletion: ChatCompletion = await openai.chat.completions.create({
       messages: context,
       model: model,
