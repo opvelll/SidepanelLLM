37860305e7854d33d46e6255cfd35830b9a1a81a
opvel <opvel.hero@gmail.com>
2024-06-05 22:56:46 +0900

    refactor: update background script to handle message types and improve code structure

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -4,9 +4,6 @@ import 'webextension-polyfill';
 reloadOnUpdate('pages/background');
 reloadOnUpdate('pages/sidepanel');
 
-import OpenAI from 'openai';
-
-import ApiKeyStorage from '@root/src/shared/storages/ApiKeyStorage';
 import getYoutubeSubtitles from './lib/YoutubeSubtitleFetcher';
 import getAllPageText from './lib/WebPageTextExtractor';
 import sendSelectionText from './lib/TextSelectionSender';
@@ -16,27 +13,20 @@ import { fetchAIChatAPI } from './lib/ChatAPI';
 
 chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch(error => console.error(error));
 
-const handleMessages = async () => {
-  const { openAIKey } = await ApiKeyStorage.get();
-  const openai = new OpenAI({ apiKey: openAIKey });
-
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log('message', message);
-    if (message.type === 'getSelectedTextRequest') {
-      sendSelectionText().then(result => sendResponse(result));
-    } else if (message.type === 'queryChatAPI') {
-      fetchAIChatAPI(openai, message.model, message.context).then(result => sendResponse(result));
-    } else if (message.type === 'getAllPageRequest') {
-      getAllPageText().then(result => sendResponse(result));
-    } else if (message.type === 'getSubtitlesRequest') {
-      getYoutubeSubtitles().then(result => sendResponse(result));
-    } else if (message.type === 'getScreenshot') {
-      captureVisibleTab(sendResponse);
-    }
-    return true;
-  });
-};
-
-handleMessages();
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  console.log('message', message);
+  if (message.type === 'getSelectedTextRequest') {
+    sendSelectionText().then(result => sendResponse(result));
+  } else if (message.type === 'queryChatAPI') {
+    fetchAIChatAPI(message.model, message.context).then(result => sendResponse(result));
+  } else if (message.type === 'getAllPageRequest') {
+    getAllPageText().then(result => sendResponse(result));
+  } else if (message.type === 'getSubtitlesRequest') {
+    getYoutubeSubtitles().then(result => sendResponse(result));
+  } else if (message.type === 'getScreenshot') {
+    captureVisibleTab(sendResponse);
+  }
+  return true;
+});
 
 console.log('background loaded');
