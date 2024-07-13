df747a06bc97bf607f68d51fb88890e0ce249efb
opvel <opvel.hero@gmail.com>
2024-07-13 22:01:52 +0900

    feat: Refactor background message handling for improved readability and maintainability

--- chrome-extension/lib/background/index.ts
+++ chrome-extension/lib/background/index.ts
@@ -4,22 +4,29 @@ import getAllPageText from './lib/WebPageTextExtractor';
 import sendSelectionText from './lib/TextSelectionSender';
 import { captureVisibleTab } from './lib/Capture';
 import { fetchAIChatAPI } from './lib/ChatAPI';
+import { MessageToBackground } from '../../../pages/sidepanel/src/types/MessageType';
 
 chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch(error => console.error(error));
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener((message: MessageToBackground, sender, sendResponse) => {
   if (import.meta.env.MODE === 'development') console.log('message', message);
 
-  if (message.type === 'getSelectedTextRequest') {
-    sendSelectionText().then(result => sendResponse(result));
-  } else if (message.type === 'queryChatAPI') {
-    fetchAIChatAPI(message.model, message.context).then(result => sendResponse(result));
-  } else if (message.type === 'getAllPageRequest') {
-    getAllPageText().then(result => sendResponse(result));
-  } else if (message.type === 'getSubtitlesRequest') {
-    getYoutubeSubtitles().then(result => sendResponse(result));
-  } else if (message.type === 'getScreenshot') {
-    captureVisibleTab(sendResponse);
+  switch (message.type) {
+    case 'queryChatAPI':
+      fetchAIChatAPI(message.model, message.context).then(result => sendResponse(result));
+      break;
+    case 'getSelectedTextRequest':
+      sendSelectionText().then(result => sendResponse(result));
+      break;
+    case 'getAllPageRequest':
+      getAllPageText().then(result => sendResponse(result));
+      break;
+    case 'getSubtitlesRequest':
+      getYoutubeSubtitles().then(result => sendResponse(result));
+      break;
+    case 'getScreenshot':
+      captureVisibleTab(sendResponse);
+      break;
   }
   return true;
 });
